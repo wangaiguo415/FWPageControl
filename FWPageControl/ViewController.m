a9271c23d67b59846be79b2163d65b7984e7b63b
Chin <23335465@qq.com>
2015-03-17 10:52:47 +0800

    add samples

--- FWPageControl/ViewController.m
+++ FWPageControl/ViewController.m
@@ -17,11 +17,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    FWPageControl *control = [[FWPageControl alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 20)];
-    [control setItemSpacing:5];
-    [control setContentInset:UIEdgeInsetsMake(9, 10, 9, 10)];
-    [control setNumberOfPages:5];
-    [self.view addSubview:control];
+    {
+        FWPageControl *control = [[FWPageControl alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 20)];
+        [control setItemSpacing:5];
+        [control setContentInset:UIEdgeInsetsMake(9, 10, 9, 10)];
+        [control setNumberOfPages:5];
+        [self.view addSubview:control];
+    }
+    {
+        FWPageControl *control = [[FWPageControl alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 100)/2.0, 200, 100, 20)];
+        [control setItemSpacing:5];
+        [control setContentInset:UIEdgeInsetsMake(9, 10, 9, 10)];
+        [control setNumberOfPages:5];
+        [control setBackgroundColor:[UIColor blueColor]];
+        [self.view addSubview:control];
+    }
+    {
+        FWPageControl *control = [[FWPageControl alloc] initWithFrame:CGRectMake((self.view.frame.size.width - 200)/2.0, 300, 200, 20)];
+        [control setItemSpacing:2];
+        [control setContentInset:UIEdgeInsetsMake(9, 10, 9, 10)];
+        [control setNumberOfPages:3];
+        control.normalColor = [UIColor blueColor];
+        control.selectedColor = [UIColor redColor];
+        [self.view addSubview:control];
+    }
     // Do any additional setup after loading the view, typically from a nib.
 }
 
